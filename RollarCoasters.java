256afc903eb278f23a70992dfbdd80ebf60390f5
Cassie-feinberg <cassandraf24@nycstudents.net>
2024-12-13 13:06:44 -0500

    12/13 Claire!

--- RollarCoasters.java
+++ RollarCoasters.java
@@ -1,2 +1,5 @@
 public class RollarCoasters extends Rides{
+    public RollarCoasters(int ageLimit, int heightLimit, int cost, boolean working, int waitTime) {
+        super(ageLimit, heightLimit, cost, working, waitTime);
+    }
 }
--- WaterRides.java
+++ WaterRides.java
@@ -1,2 +1,5 @@
 public class WaterRides extends Rides{
+    public WaterRides(int ageLimit, int heightLimit, int cost, boolean working, int waitTime) {
+        super(ageLimit, heightLimit, cost, working, waitTime);
+    }
 }
