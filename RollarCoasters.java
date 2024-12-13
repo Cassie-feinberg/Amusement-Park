abfa55c83bcaba23fce7d75bce77b4172f35e379
Cassie-feinberg <cassandraf24@nycstudents.net>
2024-12-13 13:20:33 -0500

    12/13 Claire!

--- RollarCoasters.java
+++ RollarCoasters.java
@@ -1,5 +1,27 @@
 public class RollarCoasters extends Rides{
-    public RollarCoasters(int ageLimit, int heightLimit, int cost, boolean working, int waitTime) {
-        super(ageLimit, heightLimit, cost, working, waitTime);
+
+    private final int heightOfRide;
+    private int thrillScare;
+
+    public RollarCoasters(String name, int ageLimit, int heightLimit, int cost, boolean working, int waitTime, int heightOfRide, int thrillScare) {
+        super(name, ageLimit, heightLimit, cost, working, waitTime);
+        this.heightOfRide = heightOfRide;
+        this.thrillScare = thrillScare;
+    }
+
+    public int getThrillScare() {
+        return thrillScare;
+    }
+
+    public void setThrillScare(int thrillScare) {
+        this.thrillScare = thrillScare;
+    }
+
+    public int getHeightOfRide() {
+        return heightOfRide;
+    }
+
+    public String makeNoise(){
+        return "Ahhhhhhhh!";
     }
 }
