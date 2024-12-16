ecfdcbf13667f4d44c327962ea62c747af499d0c
Cassie-feinberg <cassandraf24@nycstudents.net>
2024-12-16 12:52:50 -0500

    12/16 Claire

--- WaterRides.java
+++ WaterRides.java
@@ -1,5 +1,29 @@
 public class WaterRides extends Rides{
-    public WaterRides(String name, int ageLimit, int heightLimit, int cost, boolean working, int waitTime) {
+
+    private int splashScale;
+    private boolean raincoat;
+
+    public WaterRides(String name, int ageLimit, int heightLimit, int cost, boolean working, int waitTime, int splashScale, boolean raincoat) {
         super(name, ageLimit, heightLimit, cost, working, waitTime);
+        this.splashScale = splashScale;
+        this.raincoat = raincoat;
+    }
+
+    public boolean hasRaincoat() {
+        return raincoat;
+    }
+
+    public int getSplashScale() {
+        return splashScale;
+    }
+
+    @Override
+    public String toString() {
+        return "The splash scale of " + getName() + "is "+ getSplashScale();
+    }
+
+    @Override
+    public String makeNoise() {
+        return "Splash!!!!";
     }
 }
