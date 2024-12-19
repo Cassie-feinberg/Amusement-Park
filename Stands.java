ee97d475cdf6cb13604e4a3fff3242a175376add
Cassie-feinberg <cassandraf24@gmail.com>
2024-12-19 13:02:35 -0500

    Maya Nover 12/19

--- Stands.java
+++ Stands.java
@@ -20,7 +20,9 @@ public class Stands extends FoodSpots {
     }
 
     public static void main(String[] args){
-        Stands Marians= new Stands("Pablos", true, "Mexican", 2.99, 23.55, "9:30 AM to 8 PM", false);
-        System.out.println( Marians.toString());
+        Stands Pablos = new Stands("Pablos", false, "Mexican", 2.99, 23.55, "9:30 AM - 8PM", false);
+        Stands Gyros = new Stands("Gyros", true, "Greek", 4.75, 32.55, "10:30 AM - 6PM", false);
+        Stands Mikes = new Stands("Mikes", true, "Burger", 1.50, 25.75, "8AM to 8PM", true);
+        Stands[] stands = new Stands[]{Pablos, Gyros, Mikes};
     }
 }
