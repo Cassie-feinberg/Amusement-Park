f6dffdaab80e4108b165a21876bd1dd85bde4502
cassie <cassandrafeinberg@gmail.com>
2024-12-19 13:05:33 -0500

    runnner almost done

--- Park.java
+++ Park.java
@@ -1,9 +1,33 @@
 import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
-import java.util.random.RandomGenerator;
+import java.util.Random;
 import java.util.Scanner;
 public class Park {
     public static void main(String[] args){
+        Restaurant Marians= new Restaurant("Marians", true, "Italian", 6.57, 37.45, "9 AM to 10 PM", 17,15,false);
+        Restaurant chinese= new Restaurant("Yummies", true, "Chinese", 4.22, 40, "7 AM to 10 PM", 30,30,false);
+        Restaurant french = new Restaurant("Francois et Bruno", false, "French", 20, 110.45, "5PM to 1 AM", 5,40,true);
+        Restaurant[] restaurants = new Restaurant[]{Marians, chinese, french};
+
+        Stands[] stands = new Stands[]{};
+
+        FoodSpots[] foodSpots = new FoodSpots[]{};
+
+        RollarCoasters zoomingMountain = new RollarCoasters("Zooming Mountain", 12, 54, 5, true, 45, 200, 10);
+        RollarCoasters dash = new RollarCoasters("Dash", 4, 36, 4, true, 2, 8, 0);
+        RollarCoasters kaboom = new RollarCoasters("Kaboom", 10, 48, 3, false, 30, 8, 7);
+        RollarCoasters[] rollerCoasters = new RollarCoasters[]{zoomingMountain, dash, kaboom};
+
+        WaterRides splashies = new WaterRides("Splashies", 4, 24, 1, true, 10, 1, false);
+        WaterRides splatoon = new WaterRides("Splatoon", 10, 54, 4, false, 15, 4, false);
+        WaterRides splat = new WaterRides("Splat", 16, 60, 10, true, 30, 8, true);
+        WaterRides[] waterRides = new WaterRides[]{splashies, splatoon, splat};
+
+        Rides teacup = new Rides("Teacup Spin Time", 2, 31, 2, true, 20);
+        Rides carousel = new Rides("Whirligig", 2, 31, 2, false, 0);
+        Rides ferrisWheel = new Rides("Big Zero", 0, 0, 10, true, 40);
+        Rides[] rides = new Rides[]{teacup, carousel, ferrisWheel};
+
         Scanner user = new Scanner(System.in);
         System.out.println("Welcome to the amusement park, what is your name? ");
         String name = user.nextLine();
@@ -17,58 +41,59 @@ public class Park {
         while (stay){
             System.out.println("1. Go get food\n2. Go on a ride\nEnter the number of where you would like to go: ");
             String place = user.nextLine();
+            Random generator = new Random();
+            int randomIndex = generator.nextInt(3);
             if (place.equals("1")){
                 System.out.println("1. Go to a restaurant\n2. Go to a food stand\n Enter the number of the type of food place you would like to go to: ");
                 String food = user.nextLine();
                 if (food.equals("1")){
-
+                    System.out.println("We are going to " + restaurants[randomIndex].getName());
+                    System.out.println(restaurants[randomIndex]);
                 }
                 else if (food.equals("2")){
-
+                    System.out.println("We are going to " + stands[randomIndex].getName());
+                    System.out.println(stands[randomIndex]);
+                }
+                else {
+                    System.out.println("We are going to " + foodSpots[randomIndex].getName());
+                    System.out.println(foodSpots[randomIndex]);
                 }
             }
             else if (place.equals("2")){
                 System.out.println("1. Go on a roller coaster\n2. Go on a water ride\n Enter the number of the ride type you would like to go on: ");
                 String ride = user.nextLine();
                 if (ride.equals("1")){
-
-                    ArrayList<String> rollerCoasters = new ArrayList<String>();
-                    RollarCoasters zoomingMountain = new RollarCoasters("Zooming Mountain", 12, 54, 5, true, 45, 200, 10);
-                    rollerCoasters.add("Zooming Mountain");
-
-                    RollarCoasters dash = new RollarCoasters("Dash", 4, 36, 4, true, 2, 8, 0);
-                    rollerCoasters.add("Dash");
-
-                    RollarCoasters kaboom = new RollarCoasters("Kaboom", 10, 48, 3, false, 30, 8, 7);
-                    rollerCoasters.add("Kaboom");
-
-
-//                    System.out.println(dash);
-//                    System.out.println(rollerCoasters);
-//                    System.out.println(zoomingMountain.makeNoise());
-                    //change it later
+                    System.out.println("We are going to go ride " + rollerCoasters[randomIndex].getName());
+                    System.out.println(rollerCoasters[randomIndex]);
+                    if (rollerCoasters[randomIndex].getHeightLimit() > height){
+                        System.out.println("You are not tall enough to ride this, Sorry!");
+                    }
+                    if (rollerCoasters[randomIndex].getAgeLimit() > age){
+                        System.out.println("You are not old enough to ride this, Sorry!");
+                    }
                 }
                 else if (ride.equals("2")){
-
-                    ArrayList<String> waterRides = new ArrayList<String>();
-                    WaterRides splashies = new WaterRides("Splashies", 4, 24, 1, true, 10, 1, false);
-                    waterRides.add("splashies");
-
-                    WaterRides splatoon = new WaterRides("Splatoon", 10, 54, 4, false, 15, 4, false);
-                    waterRides.add("splatoon");
-
-                    WaterRides splat = new WaterRides("Splat", 16, 60, 10, true, 30, 8, true);
-                    waterRides.add("splat");
-
-
-//                    System.out.println(splatoon);
-//                    System.out.println(waterRides);
-//                    System.out.println(splashies.makeNoise());
-
-                    // change later
+                    System.out.println("We are going to go ride " + waterRides[randomIndex].getName());
+                    System.out.println(waterRides[randomIndex]);
+                    if (waterRides[randomIndex].getHeightLimit() > height){
+                        System.out.println("You are not tall enough to ride this, Sorry!");
+                    }
+                    if (waterRides[randomIndex].getAgeLimit() > age){
+                        System.out.println("You are not old enough to ride this, Sorry!");
+                    }
+                }
+                else {
+                    System.out.println("We are going to go ride " + rides[randomIndex].getName());
+                    System.out.println(rides[randomIndex]);
+                    if (rides[randomIndex].getHeightLimit() > height){
+                        System.out.println("You are not tall enough to ride this, Sorry!");
+                    }
+                    if (rides[randomIndex].getAgeLimit() > age){
+                        System.out.println("You are not old enough to ride this, Sorry!");
+                    }
                 }
             }
-            System.out.println("Enter 1 if you want to leave: ");
+            System.out.println("Enter 1 if you want to leave and any other number if you want to stay: ");
             String Stay = user.nextLine();
             if (Stay.equals("1")){
                 stay = false;
